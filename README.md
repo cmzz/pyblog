5843a9fb9fa80eeb171e4263b39ee2a498193269
wuzhuo <me19@qq.com>
2015-11-07 11:54:20 +0800

    补充注释

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # pyblog
-pyblog
 
-simple blog with python & flask
+简单的博客系统，Flask Web的练手项目，因此开发过程中会尽可能的完善注释与笔记
+
+# 版本
+- python 3.4
+- flask 0.10
+- 其他组件版本请参见 requirements.txt中定义
+
+# Demo
+- 暂无 
 
--- manage.py
+++ manage.py
@@ -1,22 +1,32 @@
 __author__ = 'Andy'
 
+"""
+管理脚本，可以启动控制台，启动开发服务器，执行数据库操作等
+同时可以你可在这里添加自定义的命令
+"""
+
 from app import create_app, db
 from app.models import User
 from flask_script import Manager, Shell, Server
 from flask_migrate import Migrate, MigrateCommand
 
+# 传入开发模式参数
 app = create_app("development")
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 
+# 创建一个上下文
 def make_shell_context():
     return dict(app=app, db=db, User=User)
 
 
+# 启动控制台命令
 manager.add_command("shell", Shell(make_shell_context))
+# 数据库的操作命令，如升级还原等
 manager.add_command("db", MigrateCommand)
 
+# 启动web服务器， 仅用于开发环境
 manager.add_command('runserver', Server())
 
 if __name__ == "__main__":
